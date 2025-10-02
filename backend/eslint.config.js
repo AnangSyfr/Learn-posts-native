80da946be271b093737de16dc5cae5b46c2bc525
Anang Syaifur <anangsyfr123@gmail.com>
2025-10-02 09:30:19 +0700

    feat: backend init

---
+++ backend/eslint.config.js
@@ -0,0 +1,37 @@
+const { defineConfig } = require("eslint/config");
+
+const globals = require("globals");
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+    },
+
+    extends: compat.extends(
+      "plugin:@typescript-eslint/recommended",
+      "eslint:recommended"
+    ),
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+  },
+]);
